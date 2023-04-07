a0051e7cce4a947808c7e914bd29066ec8927623
DWVoid <yshliu0321@icloud.com>
2023-04-07 15:29:14 +0800

    concurrent map/set

--- conc/set.go
+++ conc/set.go
@@ -1,1 +1,26 @@
 package conc
+
+import "sync"
+
+type Set[K comparable] struct {
+	m sync.Map
+}
+
+func (m *Set[K]) Has(key K) bool {
+	_, ok := m.m.Load(key)
+	return ok
+}
+
+func (m *Set[K]) Remove(key K) bool {
+	_, was := m.m.LoadAndDelete(key)
+	return was
+}
+
+func (m *Set[K]) Set(key K) bool {
+	_, was := m.m.LoadOrStore(key, true)
+	return was
+}
+
+func (m *Set[K]) Range(f func(key K) bool) {
+	m.m.Range(func(key, value any) bool { return f(key.(K)) })
+}
